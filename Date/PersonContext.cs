4aa6f55f76ab6542ea22c64b0d4edb6e86ee30a7
GenesisClinux <desenvolvimento@clinux.com.br>
2024-11-05 23:51:20 -0300

    Adicionar arquivos de projeto.

---
+++ Date/PersonContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Person.Models;
+
+namespace Person.Date {
+    public class PersonContext : DbContext {
+        public DbSet<Models.PersonModel> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlite("Data Source=Person.sqlite");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Routes/PersonRoute.cs
@@ -0,0 +1,58 @@
+﻿using Person.Models;
+using Person.Date;
+using Microsoft.EntityFrameworkCore;
+
+namespace Person.Routes {
+    public static class PersonRoute {
+        public static void PersonRoutes(this WebApplication app) {
+
+            var route = app.MapGroup("person");
+
+            route.MapPost("",
+                async (PersonRequest req, PersonContext context) => {
+                    var person = new PersonModel(req.name);
+                    await context.AddAsync(person);
+                    await context.SaveChangesAsync();
+                }
+            );
+
+            route.MapGet("", async (PersonContext ctx) => {
+                var people = await ctx.People.ToListAsync();
+                if (people.Count == 0) {
+                    return Results.NoContent();
+                }
+                return Results.Ok(people);
+            });
+
+            route.MapPatch("{id:guid}",
+                async (Guid id, PersonRequest req, PersonContext ctx) => {
+                    var person = await ctx.People.FirstOrDefaultAsync(x => x.Id == id);
+
+                    if (person == null) 
+                        return Results.NotFound();
+                    
+                    person.ChangeName(req.name);
+
+                    await ctx.SaveChangesAsync();
+
+                    return Results.Ok(person);
+
+                });
+
+            route.MapDelete("{id:guid}",
+                    async (Guid id, PersonContext ctx) => {
+                        var person = await ctx.People.FirstOrDefaultAsync(x => x.Id == id);
+
+                        if (person == null)
+                            return Results.NotFound();
+
+                        ctx.People.Remove(person);
+
+                        await ctx.SaveChangesAsync();
+                        return Results.NoContent();
+                    }
+
+                );
+        }
+    }
+}
